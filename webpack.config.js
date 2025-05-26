d9caaa8bd1b769c2234fd648e80d367963ec6dbb
clxm-29 <villainclement29@gmail.com>
2025-05-26 11:23:37 +0200

    build and serve script added

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
-module.exports = { 
-// 1 
-entry: './src/index.js', 
-// 2 
-output: { 
-path: '/dist', 
-filename: 'bundle.js' 
-}, 
-// 3 
-devServer: { 
-static: './dist' 
-} 
+const path = require("path");
+module.exports = {
+
+    // 1 
+    entry: './src/index.js',
+    // 2 
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    // 3 
+    devServer: {
+        static: './dist'
+    },
+
+
 }; 
